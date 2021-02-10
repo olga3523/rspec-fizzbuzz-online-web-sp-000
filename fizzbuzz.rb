70fa4b5e02a0d88f1221dc5c6d106ea902552ab6
olga gomelskiy <olichka23.85@gmail.com>
2021-02-10 04:57:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,5 @@
 # Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+#
+if 9/3
+  puts"Fizz" See README.md for instructions on how to do this
+end
